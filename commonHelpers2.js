ca0918dd3574b8deb30c41c6f084668ab6581d75
AntoXa32 <151663096+AntoXa32@users.noreply.github.com>
2024-05-01 11:20:43 +0000

    Deploying to gh-pages from @ AntoXa32/goit-js-hw-10@2b0ee84c45a6c40d707b9c9bb49b90d2c7dad46e 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",function(o){o.preventDefault();const t=parseInt(this.elements.delay.value),s=this.elements.state.value;new Promise((e,m)=>{s==="fulfilled"?setTimeout(()=>e(t),t):s==="rejected"&&setTimeout(()=>m(t),t)}).then(e=>{i.success({title:"✅ Fulfilled promise",message:`Fulfilled promise in ${e}ms`})},e=>{i.error({title:"❌ Rejected promise",message:`Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=commonHelpers2.js.map
